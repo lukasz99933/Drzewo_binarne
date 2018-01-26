90b41f441e57226c3004651aa0a83c1c0b0a15a7
lukasz99933 <lukasz8126700@gmail.com>
2018-01-26 23:10:23 +0100

    Formatowanie kodu.

--- babelkowe.cpp
+++ babelkowe.cpp
@@ -1,119 +1,60 @@
-//Sortowanie bąbelkowe.
-
-#include<iostream>
-
-#include<cstdlib>
-
-
+// Sortowanie bąbelkowe.
+#include <cstdlib>
+#include <iostream>
 
 using namespace std;
 
-
-
 class Tablica
-
 {
-
-    int n=20; //rozmiar
-
-    int *tablica=new int[n];
+  int n = 20; // rozmiar
+  int* tablica = new int[n];
 
 public:
-
-    void sortowanie(int i); //Metoda do sortowania tablicy
-
-    void wyswietlanie(); //Metoda do wyświetlania tablicy.
-
-    Tablica(); //konstruktor, który generuje losowe wartości do tablicy
-
-    ~Tablica();
-   
-
+  void sortowanie(int i); // Metoda do sortowania tablicy
+  void wyswietlanie();    // Metoda do wyświetlania tablicy.
+  Tablica(); // konstruktor, który generuje losowe wartości do tablicy
+  ~Tablica();
 };
 
-
-
 Tablica::Tablica()
-
 {
-
-    for( int i=0;i<n;i++)
-
-    {
-
-        tablica[i]=(rand()%100);
-
-    }
-
+  for (int i = 0; i < n; i++) {
+    tablica[i] = (rand() % 100);
+  }
 }
 
 Tablica::~Tablica()
 {
-    delete[] tablica;
+  delete[] tablica;
 }
 
-
-
-void Tablica::sortowanie(int i)
-
+void
+Tablica::sortowanie(int i)
 {
-
-    if(i<n)
-
-    {
-
-        for( int j=0; j<n-1;j++ )
-
-        {
-
-            if(tablica[j]>tablica[j+1])
-
-            {
-
-                swap(tablica[j], tablica[j+1]);
-
-            }
-
-        }
-
-        sortowanie(i+1);
-
+  if (i < n) {
+    for (int j = 0; j < n - 1; j++) {
+      if (tablica[j] > tablica[j + 1]) {
+        swap(tablica[j], tablica[j + 1]);
+      }
     }
-
-    else return;
-
+    sortowanie(i + 1);
+  } else
+    return;
 }
 
-
-
-void Tablica::wyswietlanie()
-
+void
+Tablica::wyswietlanie()
 {
-
-    for(int k=0;k<n;k++)
-
-    {
-
-        cout<<tablica[k]<<"\t";
-
-    }
-
+  for (int k = 0; k < n; k++) {
+    cout << tablica[k] << "\t";
+  }
 }
 
-
-
-int main()
-
+int
+main()
 {
-
-    Tablica K;
-
-    K.sortowanie(0);
-
-    K.wyswietlanie();
-
-    return 0;
-
+  Tablica K;
+  K.sortowanie(0);
+  K.wyswietlanie();
+  return 0;
 }
-
-
